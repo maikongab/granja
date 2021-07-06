82898cfd919f1593ca633d76b7056101320e15d8
Maikon gabriel Win <maikon1998gab@gmail.com>
2021-07-06 18:03:24 -0300

    create database use sqlite

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import express from "express";
+
+import "./database"
+
+
+const app = express();
+
+
+app.listen(3333,() => console.log("server is running on port 3333"));

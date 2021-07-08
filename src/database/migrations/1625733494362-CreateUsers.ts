1fd308fff2dfd8e09c3d7aca8e1ed57b7269f4b0
Maikon gabriel Win <maikon1998gab@gmail.com>
2021-07-08 05:58:17 -0300

    create table

---
+++ src/database/migrations/1625733494362-CreateUsers.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table, useContainer} from "typeorm";
+
+export class CreateUsers1625733494362 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns:[
+                    {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "user",
+                    type: "varchar"
+                },
+                {
+                    name: "email",
+                    type:"varchar"
+                },
+                {
+                    name:"admin",
+                    type:"boolean",
+                    default: false
+                },
+                {
+                    name: "password",
+                    type: "varchar"
+                },
+                {
+                    name:"created_at",
+                    type:"timestamp",
+                    default:"now()"
+                },
+                {
+                    name:"updated_at",
+                    type:"timestemp",
+                    default:"now()"
+                }
+            ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

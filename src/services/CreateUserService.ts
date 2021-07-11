170862d739cb9fb991c2c37d9b823521cece00b7
Maikon Gabriel <62940651+Maikongab20@users.noreply.github.com>
2021-07-11 05:59:53 -0300

    done service

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { hash } from "bcryptjs"
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 interface IUserRequest{
     name: string;
@@ -11,8 +12,32 @@ class createUserService{
 
     async execute({name, email, admin, password} : IUserRequest){
 
+        const usersRepositories = new UsersRepositories();
+
+        if(!email){
+            throw new Error("email incorrect")
+        }
+
+        const userAlredyExists =await usersRepositories.findOne({
+            email
+        });
+
+        if(userAlredyExists){
+            throw new Error("user alredy exists");
+        }
 
         const passwordHash =  await hash(password, 8);
+
+        const user = usersRepositories.create({
+            name,
+            email,
+            admin,
+            password
+        })
+
+        await usersRepositories.save(user);
+        
+        return user;
     }
 }
 
